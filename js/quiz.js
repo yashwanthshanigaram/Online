2516d0ec2f0a12cee1ba4c5158260e86aa4cdf6c
yashwanthshanigaram <yashwanthshanigaram1998@gmail.com>
2020-09-11 20:12:41 +0530

    First Commit

---
+++ js/quiz.js
@@ -0,0 +1,104 @@
+let quiz=[
+    {
+        question:"react is a ?",
+        option:[
+            "javascript library",
+            "framework",
+            "both of the above",
+            "none of the above",
+        ],
+        answer:1,
+    },
+    {
+        question:" react js covers covers_____",
+        option:[
+            "User Interface layer in an application",
+            "Data layer in an appliaction",
+            "both of the above",
+            " none of the above",
+            
+        ],
+        answer:1,
+    },
+    {
+        question:"AngularJs directives are used in",
+        option:[
+            "Module",
+            "Controller",
+            "Service",
+            "View",   
+        ],
+        answer:4,
+    },
+    {
+        question:"which file is responsible for startup of angular2 project ?",
+        option:[
+            "main.ts",
+            "index.js",
+            "app.ts",
+            "angular.cli.json",   
+        ],
+        answer:1,
+
+    },
+    {
+        question:"Which of the following class is required to be added to form tag to make it horizontal?",
+        option:[
+            ".horizontal",
+            ".form-horizontal",
+            ".horizontal",
+            "none of the above",   
+        ],
+        answer:2,
+    },
+    {
+        question:"which of the following bootstrap style is used for a basic button group?",
+        option:[
+            ".btn-group",
+            ".btn-toolbar",
+            ".btn-group-lg",
+            ".btn-group-vertical",   
+        ],
+        answer:1,
+    },
+    {
+        question:"Which directive initializes an angular js application?",
+        option:[
+            "ng-init",
+            "ng-app",
+            "ngsrc",
+            "ng-start",   
+        ],
+        answer:2,
+    },
+    {
+        question:"Which of the following will return the type of the arguments passed to a function ?",
+        option:[
+            "using getType function",
+            "using typeof operator",
+            "Both of the above",
+            "None",   
+        ],
+        answer:2,
+    },
+    {
+        question:"AngularJs expressions are written using",
+        option:[
+            "(expression)",
+            "{{expression}}",
+            "{{{expression}}}",
+            "[expression]",   
+        ],
+        answer:2,
+    },
+    {
+        question:"Angular is based on ",
+        option:[
+            "MVC architecture",
+            "Decorator pattern",
+            "MVVM architectural pattern",
+            "observer pattern",   
+        ],
+        answer:3,
+    }
+]
